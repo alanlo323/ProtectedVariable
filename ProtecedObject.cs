2d7e15ba28b107dd103102705ff371c37fb4b674
AlanLo <alanlo323z@gmail.com>
2020-09-05 17:19:21 +0800

    Add base class

--- ProtecedObject.cs
+++ ProtecedObject.cs
@@ -4,8 +4,50 @@ using System.Text;
 
 namespace ProtectedVariable
 {
-    class ProtecedObject
+    public interface ProtecedObjectCallBack
     {
-        public object Value { get; set; }
+        void OnValueInvalid();
+    }
+
+    public abstract class ProtecedObject<T>
+    {
+        private object Factor1 { get; set; }
+        private object Factor2 { get; set; }
+        public T Value
+        {
+            get
+            {
+                if (IsValueValid())
+                {
+                    if (ProtecedObjectCallBack != null)
+                    {
+                        ProtecedObjectCallBack.OnValueInvalid();
+                    }
+                }
+                IsValueValid();
+                return default;
+            }
+            set
+            {
+
+            }
+        }
+
+        private ProtecedObjectCallBack ProtecedObjectCallBack { get; set; }
+
+        public ProtecedObject(ProtecedObjectCallBack ProtecedObjectCallBack = null)
+        {
+            this.ProtecedObjectCallBack = ProtecedObjectCallBack;
+        }
+
+        public ProtecedObject(T Value, ProtecedObjectCallBack ProtecedObjectCallBack = null) : this(ProtecedObjectCallBack)
+        {
+            this.Value = Value;
+        }
+
+        protected abstract bool IsValueValid();
+
+        public abstract void RefreshValue();
+
     }
 }
